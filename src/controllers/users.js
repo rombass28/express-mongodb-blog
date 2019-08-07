d41d12e7af60f28399b07ccf541ed3302a37c620
evyatar <evyatar1919@gmail.com>
2019-08-07 18:55:41 +0300

    for adi #2

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,30 +1,27 @@
-const ObjectId = require('mongodb').ObjectId;
-const db = require('../db');
 const User = require('../models/user');
 
 module.exports = {
 	getAll: (req, res) => {
-		db().collection('users')
-			.find({})
-			.toArray()
+		User.find()
 			.then(users => res.json(users));
 	},
 	createUser: (req, res) => {
 		const user = new User(req.body);
 		user.save()
-			.then(user => res.status(201).json(user));
+			.then(user => res.status(201).json(user))
+			.catch(err => res.status(400).json(err));
 	},
 	deleteUser: (req, res) => {
-		db().collection('users')
-			.findOne({_id: ObjectId(req.params.id)})
+		User.findById(req.params.id)
 			.then(user => {
-				if(user){
-					db().collection('users')
-						.deleteOne({_id: ObjectId(user._id)})
+				if(user) {
+					user.delete()
 						.then(() => res.status(204).send())
+						.catch(() => res.status(500).send());
 				} else {
 					res.status(404).send();
 				}
-			});
+			})
+		;
 	}
 };
--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-	email: String,
-	password: String
+	email: {
+		type: String,
+		minlength: 8,
+		maxlength: 65,
+		required: true
+	},
+	password:{
+		type: String,
+		required: true
+	},
+	age: {
+		type: Number,
+		required: true,
+		min: 18,
+		max: 120
+	}
 });
 
 const User = mongoose.model('user', UserSchema);
